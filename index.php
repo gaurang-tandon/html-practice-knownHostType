9a9bf75395ec29314271c17c8350693b7d37d7cf
Gaurang Tandon <gaurang.tandon@gmail.com>
2021-04-11 02:28:48 +0530

    Initial Commit - Div 1, 2, 3 Working

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="bootstrap_4.6.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="styleSheet.css">
+	</head>
+    <body>
+    <div id="header" class="conatiner-fluid">
+        <div class="row">
+            <div class="text-center col-xl-6 col-lg-6 col-md-12">
+                AAAAA
+            </div>
+            <div class="text-center col-xl-6 col-lg-6 col-md-12">
+                BBBBB
+            </div>
+        </div>
+        
+    </div>
+
+<!----------------------------- TOP NAVBAR START --------------------------------->
+
+    <div id="div_2" class="conatiner-fluid bg-light">
+        <div class="d-flex justify-content-center container">
+        
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown link
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown link
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown link
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown link
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li>
+                </ul>
+            </div>
+            </nav>
+        </div>
+    </div>
+
+<!----------------------------- TOP NAVBAR END --------------------------------->
+
+
+<!----------------------------- JUMBOTRON START --------------------------------->
+
+    <div id="div_3" class="text-center conatiner-fluid">
+    <img src="img/slider_3.jpg" class="img-fluid" alt="Responsive image">
+    <div class="centered">
+        <h1>Centered Heading Over The Image</h1>
+    </div>
+    <div class="center-bottom">
+        <p>A Quick Brown Fox Jumps Over The Lazy Dog. A Quick Brown Fox Jumps Over The Lazy Dog. 
+            A Quick Brown Fox Jumps Over The Lazy Dog. A Quick Brown Fox Jumps Over The Lazy Dog. 
+            A Quick Brown Fox Jumps Over The Lazy Dog. A Quick Brown Fox Jumps Over The Lazy Dog.</p>
+    </div>
+    </div>
+
+<!----------------------------- JUMBOTRON END --------------------------------->
+
+    <div id="div_4" class="conatiner-fluid">
+        <div class="container">
+        D
+        </div>
+    </div>
+    <div id="div_5" class="conatiner-fluid">
+        <div class="container">
+        E
+        </div>
+    </div>
+    <div id="div_6" class="conatiner-fluid">
+        <div class="container">
+        F
+        </div>
+    </div>
+    <div id="div_7" class="conatiner-fluid">
+        <div class="container">
+        G
+        </div>
+    </div>
+    <div id="div_8" class="conatiner-fluid">
+        <div class="container">
+        H
+        </div>
+    </div>
+    <div id="div_9" class="conatiner-fluid">
+        <div class="container">
+        I
+        </div>
+    </div>
+    <div id="div_10" class="conatiner-fluid">
+        <div class="container">
+        J
+        </div>
+    </div>
+    <div id="div_11" class="conatiner-fluid">
+        <div class="container">
+        K
+        </div>
+    </div>
+    <div id="div_12" class="conatiner-fluid">
+        <div class="container">
+        L
+        </div>
+    </div>
+    <div id="div_13" class="conatiner-fluid">
+        <div class="container">
+        M
+        </div>
+    </div>
+    <div id="div_14" class="conatiner-fluid">
+        <div class="container">
+        N
+        </div>
+    </div>
+    <div id="div_15" class="conatiner-fluid">
+        <div class="container">
+        O
+        </div>
+    </div>
+    <div id="footer" class="conatiner-fluid">
+        <div class="container">
+        P
+        </div>
+    </div>
+
+
+
+
+
+    <script src="jQuery_3.6.0.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="bootstrap_4.6.0/js/bootstrap.min.js"></script>
+    </body> 
+</html>
